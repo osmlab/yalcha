0f439da0f0c98316fa23c7caeeb4524d6afeb4b3
Paul Mach <paul@mach.org>
2018-06-17 10:07:36 -0700

    time marshals to iso8601 utc

---
+++ osm/helpers_test.go
@@ -0,0 +1,39 @@
+package osm
+
+import (
+	"testing"
+	"time"
+)
+
+func TestTimeOSMString(t *testing.T) {
+	newYork, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		t.Fatalf("invalid timezone: %v", err)
+	}
+
+	cases := []struct {
+		name     string
+		time     time.Time
+		expected string
+	}{
+		{
+			name:     "iso 8601 format",
+			time:     time.Date(2012, 1, 1, 0, 0, 0, 0, time.UTC),
+			expected: "2012-01-01T00:00:00Z",
+		},
+		{
+			name:     "always UTC",
+			time:     time.Date(2012, 1, 1, 0, 0, 0, 0, newYork),
+			expected: "2012-01-01T05:00:00Z",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			tosm := TimeOSM(tc.time)
+			if v := tosm.String(); v != tc.expected {
+				t.Errorf("incorrect format: %v", v)
+			}
+		})
+	}
+}
