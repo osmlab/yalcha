d8bbd043253365be7d2a57d7865ad207a63f057b
hesidoryn <heorhi.sidoryn@gmail.com>
2018-07-13 14:20:41 +0300

    Update deploy

--- deploy/gomap/deploy.sh
+++ deploy/gomap/deploy.sh
@@ -1,4 +1,1 @@
-cd ../../migrations
-goose postgres "host=gis.cesfozorknmw.us-west-2.rds.amazonaws.com user=hesidoryn password=hesidoryn dbname=gis sslmode=disable" up
-cd ../deploy/yalcha-server
 up
